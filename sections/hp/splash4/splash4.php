6da9dc6cdbb361ec986e2c702882756ce4750393
alwaysbrightblue <gaberator@mac.com>
2014-10-06 17:29:04 -0400

    working on vertical centering of items within the sections

--- sections/hp/splash4/splash4.php
+++ sections/hp/splash4/splash4.php
@@ -4,18 +4,18 @@
 // for production, you would probably clean this up a bit, but this works for 
 // getting the idea out. - Gabe Lloyd, Oct 6 2014 ?>
 
-<div class="row-fluid padding-left-none padding-right-none" id="container">
+<div class="container-fluid padding-left-none padding-right-none" id="container">
 <div class="splash-wrap" id="splash4">
 	<div class="container">
+	<div class="col-md-4 vcenter">
 
-	<div class="col-md-6">
-	<div id="splash-left">
-	<h1 class="text-center">Open Opportunities has a <span class="highlight-complement1">solution</span> for you.</h1>
+	<h1 class="text-center tall-text text-on-dark ">Open Opportunities has a <span class="highlight-complement1">solution</span> for you.</h1>
 	
 
-	<div class="text-center"><a href="#"><button class="btn btn-midas" id="center">Learn More</button></a></div>
-	</div><!-- /#splash-left -->
-	</div><!-- /col-md-4 -->
-</div><!-- /#splash -->
+	<div class="text-center"><a href="#"><button class="btn btn-midas" id="center">View All Opportunities</button></a></div>
+
+	</div><!-- /.col-md-4 -->
+
 </div><!-- /.container -->
-</div><!-- /.row-fluid-->
+</div><!-- /.splash-wrap -->
+</div><!-- /.container-fluid-->

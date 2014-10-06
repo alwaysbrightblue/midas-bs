f9ed2306b0dc8ac55fec334841d4db1c7f77d8a9
alwaysbrightblue <gaberator@mac.com>
2014-10-06 15:43:58 -0400

    Clean up section 2 - scenario1.  Added white translucent fade to background, highlights and title styles.

--- sections/hp/scenario1/scenario1.php
+++ sections/hp/scenario1/scenario1.php
@@ -5,23 +5,24 @@
 // getting the idea out. - Gabe Lloyd, Oct 6 2014 ?>
 
 <div class="container-fluid padding-left-none padding-right-none" id="container">
-<div class="splash-wrap" id="scenario1">
 
-<div id="splash-center-top" class="splash-text">
-	<h1 class="text-center">Hassle Free Collaboration</h1>
-</div><!-- /#splash-center -->
+<div class="splash-wrap" id="scenario1">
+<div class="trans-cover-white">
+
+	<h1 class="text-center highlight-trans box-pad-b box-pad-t">Hassle Free Collaboration</h1>
+
 
 	<div id="splash-left">
-	<p class="lead text-center">Work with federal employees on projects you are passionate about.</p>
-	<div class="text-center"><a href="#"><button class="btn btn-midas" id="center">Learn More</button></a></div>
+	<p class="lead text-center"><span class="highlight-complement2">Problem:</span> You need volunteers for an event, but cannot use public employees.</p>
 	</div><!-- /#splash-left -->
 
 	<div id="splash-right">
-	<p class="lead text-center">Work with federal employees on projects you are passionate about.</p>
-	<div class="text-center"><a href="#"><button class="btn btn-midas" id="center">Learn More</button></a></div>
+	<p class="lead text-center"><span class="highlight-complement1">Solution:</span> Post a Work Group request to thousands of interested federal employees.</p>
+	<div class="text-center"><a href="#"><button class="btn btn-midas" id="center">Post a Work Group Request</button></a></div>
 	</div><!-- /#splash-left -->
 
 </div><!-- /.splash-wrap-->
+</div><!-- /.trans-cover-white -->
 </div><!-- /.container-fluid-->
 
 <!-- we have to restart the container class here because we have a closing div tag in the footer.php file -->

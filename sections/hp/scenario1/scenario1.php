1ad47368f93e7349ab3a67ecf94c6f9829900738
alwaysbrightblue <gaberator@mac.com>
2014-10-06 18:34:08 -0400

    adjust layout on scenario 1

--- sections/hp/scenario1/scenario1.php
+++ sections/hp/scenario1/scenario1.php
@@ -12,11 +12,11 @@
 	<h1 class="text-center highlight-trans box-pad-b box-pad-t">Hassle Free Collaboration</h1>
 
 	<div class="container">
-	<div class="col-md-5 splash-box">
+	<div class="col-md-5 col-sm-5 splash-box">
 	<p class="lead text-center tall-text"><span class="highlight-complement2">Problem:</span> You need volunteers for an event, but cannot use public employees.</p>
 	</div><!-- /#splash-left -->
 
-	<div class="col-md-5 splash-box pull-right">
+	<div class="col-md-5 col-sm-5 splash-box pull-right">
 	<p class="lead text-center tall-text"><span class="highlight-complement1">Solution:</span> Post a Work Group request to thousands of interested federal employees.</p>
 	<div class="text-center"><a href="#"><button class="btn btn-midas" id="center">Post a Work Group Request</button></a></div>
 	</div><!-- /#splash-left -->

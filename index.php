651c5b1753e91f008da7f363fa1724b06b20a46f
alwaysbrightblue <gaberator@mac.com>
2014-10-06 16:51:59 -0400

    create 4th row.

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 	<?php include $docRoot.'/sections/hp/splash1/splash1.php'; // this is the main splash image.  can change to carousel?>
 	<?php include $docRoot.'/sections/hp/scenario1/scenario1.php'; // this is a scenario section?>
 	<?php include $docRoot.'/sections/hp/splash3/splash3.php'; // this is a info section?>
+	<?php include $docRoot.'/sections/hp/splash4/splash4.php'; // this shows existing project previews?>
 	<div class="container-fluid padding-left-none padding-right-none" id="container">
 
 <?php include('footer.php'); ?> <?php // ending div tag for .container is in here ?>
---
+++ sections/hp/splash4/splash4.php
@@ -0,0 +1,21 @@
+<?php $docRoot = getenv("DOCUMENT_ROOT"); ?>
+
+<?php // for mock up purposes only, I'm containing each section as a module.
+// for production, you would probably clean this up a bit, but this works for 
+// getting the idea out. - Gabe Lloyd, Oct 6 2014 ?>
+
+<div class="row-fluid padding-left-none padding-right-none" id="container">
+<div class="splash-wrap" id="splash4">
+	<div class="container">
+
+	<div class="col-md-6">
+	<div id="splash-left">
+	<h1 class="text-center">Open Opportunities has a <span class="highlight-complement1">solution</span> for you.</h1>
+	
+
+	<div class="text-center"><a href="#"><button class="btn btn-midas" id="center">Learn More</button></a></div>
+	</div><!-- /#splash-left -->
+	</div><!-- /col-md-4 -->
+</div><!-- /#splash -->
+</div><!-- /.container -->
+</div><!-- /.row-fluid-->

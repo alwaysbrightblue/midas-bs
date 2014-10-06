e8932a020797963eb6822fec6e2d4fb172d30cc9
alwaysbrightblue <gaberator@mac.com>
2014-10-06 15:46:46 -0400

    Create splash 3

--- index.php
+++ index.php
@@ -9,5 +9,5 @@
 
 	<?php include $docRoot.'/sections/hp/splash1/splash1.php'; // this is the main splash image.  can change to carousel?>
 	<?php include $docRoot.'/sections/hp/scenario1/scenario1.php'; // this is a scenario section?>
-
+	<?php include $docRoot.'/sections/hp/splash3/splash3.php'; // this is a info section?>
 <?php include('footer.php'); ?> <?php // ending div tag for .container is in here ?>

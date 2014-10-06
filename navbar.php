3898cb6cfe9074387eaea03b7c40482c6ad5f703
alwaysbrightblue <gaberator@mac.com>
2014-10-05 21:58:23 -0400

    Navbar html updated to match midas

--- navbar.php
+++ navbar.php
@@ -1,15 +1,9 @@
  <nav class="navbar navbar-default" role="navigation">
       <div class="container">
-        <div class="row">
+      
         
         <div class="navbar-header">
-          <div class="btn-container">
-          <div class="visible-xs-inline">
-          <button type="button" class="ask-toggle collapsed" data-toggle="collapse" data-target=".ask-the-expert">
-            <span class="sr-only">Ask The Expert Navigation</span>
-            <span class="q"><i class="fa fa-question fa-2x"></i></span>
-          </button>
-        </div>
+          
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
@@ -22,14 +16,8 @@
           
          
           </div> <!-- /.navbar-brand -->
-        <div class="navbar-right">
-          
-        <?php 
-        // Insert the Find a Dealer Above the collapse nav so it shows in the header at all times
-        include('sections/finddealer.php'); 
-        ?>
-        </div>
-        </div>
+       
+       
       </div> <!-- /.row -->
         <div id="main-nav">
         

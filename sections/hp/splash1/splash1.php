44a724d9ce43ca4b39c6d8186e18f03cd331b6cf
alwaysbrightblue <gaberator@mac.com>
2014-10-08 16:41:51 -0400

    Change background image on section 1.  Remove files not being used.

--- sections/hp/splash1/splash1.php
+++ sections/hp/splash1/splash1.php
@@ -2,7 +2,9 @@
 
 <?php // for mock up purposes only, I'm containing each section as a module.
 // for production, you would probably clean this up a bit, but this works for 
-// getting the idea out. - Gabe Lloyd, Oct 6 2014 ?>
+// getting the idea out. - Gabe Lloyd, Oct 6 2014 
+// stylesheet is located at /less/splash.less and compiled to /assets/styles/application.css
+?>
 
 <div class="container-fluid padding-left-none padding-right-none" id="container">
 <div class="splash-wrap" id="splash1">

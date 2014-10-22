416034d1926049d349e22c2ce2ce1c7ad7f3ec75
alwaysbrightblue <gaberator@mac.com>
2014-10-22 14:05:38 -0400

    Clean up hero sections.

--- sections/hp/splash3/splash3.php
+++ sections/hp/splash3/splash3.php
@@ -8,24 +8,26 @@
 <div class="inline-block-center">
 			
 			<div class="hero-wrapper">
-		      <div class="hero-widget well" id="allstar">
-		            <a href="#">
-		            <div class="icon"><i class="fa fa-clock-o"></i></div> <!-- icon -->
-		            <div class="text"><var>Save Time</var></div></a><!-- text -->
-		        </div> <!-- hero-widget well -->
-			    </div> <!-- hero-wrapper -->
+			  <!--
+			  <div class="hero-widget well" id="savetime">
+			        <a href="#">
+			        <div class="icon"><i class="fa fa-clock-o"></i></div> --> <!-- icon -->
+			  <!--      <div class="text"><var>Save Time</var></div></a> --> <!-- text -->
+			  <!--  </div> --> <!-- hero-widget well -->
+			  
+			</div> <!-- hero-wrapper -->
 			
 	 
-	    	<div class="hero-wrapper">
-	      <div class="hero-widget well" id="facebook">
-	            <a href="#" target="_blank">
-	            <div class="icon"><i class="fa fa-comments"></i></i></div>
-	            <div class="text"><var>Make Connections</var></div></a>
-	            </div> <!-- hero-wrapper -->
-	       </div>
+			<div class="hero-wrapper">
+			<div class="hero-widget well" id="comments">
+			    <a href="#" target="_blank">
+			    <div class="icon"><i class="fa fa-comments"></i></i></div>
+			    <div class="text"><var>Make Connections</var></div></a>
+			    </div> 
+			</div><!-- hero-wrapper -->
 	    
 	    	<div class="hero-wrapper">
-	        <div class="hero-widget well" id="twitter">
+	        <div class="hero-widget well" id="chart">
 	            <a href="#" target="_blank">
 	            <div class="icon"><i class="fa fa-line-chart"></i></div>
 	            <div class="text"><var>Make a Difference</var></div></a>
